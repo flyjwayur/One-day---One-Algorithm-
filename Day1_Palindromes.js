e393bf2c31fc2349b9f6f3bdc6ac6746f2763a58
HyeSoo <flyjwayur@gmail.com>
2016-03-29 10:00:28 +0300

    Day1 - check for Palindromes(adding a comment)

--- Day1_Palindromes.js
+++ Day1_Palindromes.js
@@ -1,5 +1,6 @@
 function palindrome(str) {
   // remove all non-alphanumeric characters
+  //(replace(/\W|_/g, '') can work as same as replace(/[^A-Za-z0-9]/g,''))
   var alphaStr = str.toLowerCase().replace(/[^A-Za-z0-9]/g,'');
   // reverse the alphaStr
   var reverStr = alphaStr.split('').reverse().join('');
